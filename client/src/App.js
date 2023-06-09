39a921a874a7c5c167856103ff84797dfa97fb7d
nvacan3 <nicolas.b.moise@gmail.com>
2023-06-09 12:29:14 -0400

    completes level 0

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const movies = [
+    { title: "Mean Girls" },
+    { title: "Hackers" },
+    { title: "The Grey" },
+    { title: "Sunshine" },
+    { title: "Ex Machina" },
+  ];
+  const content = movies.map((movie) => {
+    return (
+      <div key={movie} className="App">
+        <h3>{movie.title}</h3>
+      </div>
+    );
+  });
+  return <div className="App">{content}</div>;
 }
 
 export default App;
